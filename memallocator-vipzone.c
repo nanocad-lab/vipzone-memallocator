416a7a10d8247341188f63efbbd5e2e40dad5e87
Mark Gottscho <mgottscho@ucla.edu>
2013-01-16 19:00:26 -0800

    Updated Makefile to compile memallocator-vipzone against glibc-vipzone, which installs to /vipzone-tools. Also added support for vip_malloc() to memallocator-vipzone, and clarified runtime arguments.

--- memallocator-vipzone.c
+++ memallocator-vipzone.c
@@ -23,7 +23,7 @@
 #define _VIP_UTIL_HI 0x0020
 
 //From Internet somewhere
-int check_for_keyboard_input() {
+/*int check_for_keyboard_input() {
 	fd_set rfds;
 	struct timeval tv;
 	int retval = 0;
@@ -40,9 +40,16 @@ int check_for_keyboard_input() {
         if (retval == -1)
             printf("error: select()\n");
 	return retval;
-}
+}*/
 
-// Args: Number of MB to allocate, number of seconds to sleep between iterations. Both ints.
+/* Args: 
+	[1] Number of MB to allocate
+	[2] Number of seconds to sleep between iterations. Both ints.
+	[3] Allocator to use: g = malloc, m = mmap, v = vip_mmap (when applicable)
+	[4] Access pattern: n = sequential, s = staggered, r = random
+	[5] Write or read: w = write, r = read
+	[6] Number of iterations
+	*/
 int main(const int argc, const char *argv[]) {
 	unsigned long MB_to_alloc = 0; //How many MB we want to use for the test. Command-line argument.
 	unsigned long page = 0; //for iterating through all allocated pages
@@ -50,9 +57,7 @@ int main(const int argc, const char *argv[]) {
 	unsigned long val = 0; //dummy variable
 	unsigned long word = 0; //for iterating through words in a page
 	unsigned long **page_array = NULL; //to store pointers to each page we allocate
-	char choice = '\0'; //for simple keyboard input
-	int iter = 1; //counter
-	int allocator_choice = 0; //0: malloc, 1: mmap, 2: vip_mmap
+	int allocator_choice = 0; //0: malloc, 1: mmap, 2: vip_malloc, 3: vip_mmap
 	int stagger = 0;
 	int staggerFlag = 0;
 	int randomFlag = 0;
@@ -60,33 +65,98 @@ int main(const int argc, const char *argv[]) {
 	unsigned long wordsPerPage = PAGE_SIZE/sizeof(val);
 	int writeFlag = 0;
 	int sleepTime = 0;
+	int numIters = 0;
+	int iter = 1;
 
 	srand(time(NULL));
 
 	//Check arguments
-	if (argc != 3) {
-		printf("Error: Need to specify amount of memory to hog in MB, and cycle sleep time in seconds (>= 0). Terminating.\n");
+	if (argc != 7) {
+		fprintf(stderr, "Invalid number of arguments: (1) Amount of memory to hog in MB, (2) Number of sleep seconds between iterations, (3) Allocator to use (M[malloc], m[mmap], V[vip_malloc], v[vip_mmap]), (4) Access pattern (n[sequential], s[staggered], r[random]), (5) Write or read (w[write], r[read]) (6) Number of iterations\n");
 		return 1;
 	}
 
-	//User: malloc() or vip_mmap()?
-	choice = '\0';
-	do {
-		printf("Use GLIBC malloc(), syscall mmap, or syscall vip_mmap for memory allocation? (g/m/v): ");
-		choice = getchar();
-		getchar();
-	} while (choice != 'g' && choice != 'm' && choice != 'v');
+	//get arg 1
+	MB_to_alloc = (unsigned long) atol(argv[1]); //Be nice, I didn't bother validating input ><
+	printf("will alloc = %lu MB\n", MB_to_alloc);
 
-	if (choice == 'v') //use vip_mmap()
-		allocator_choice = 2;
-	else if (choice == 'm') //use mmap()
+	//get arg 2
+	sleepTime = (int) atol(argv[2]);
+	if (sleepTime < 0) {
+		fprintf(stderr, "Sleep time was < 0s, setting to 0s.\n");
+		sleepTime = 0;
+	}
+	printf("sleep time = %d s\n", sleepTime);
+
+
+	//get arg 3
+	if (argv[3][0] == 'v') { //use vip_mmap()
+		allocator_choice = 3;
+		printf("allocator = vip_mmap\n");
+	} else if (argv[3][0] == 'm') { //use mmap()
 		allocator_choice = 1;
-	else
+		printf("allocator = mmap\n");
+	} else if (argv[3][0] == 'M') {
 		allocator_choice = 0; //use malloc()
+		printf("allocator = malloc\n");
+	} else if (argv[3][0] == 'V' ) {  //use vip_malloc()
+		allocator_choice = 2;
+		printf("allocator = vip_malloc\n");
+	} else {
+		fprintf(stderr, "Bad allocator choice\n");
+		return 1;
+	}
 
+	//get arg 4
+	if (argv[4][0] == 's') { //stagger
+		staggerFlag = 1;
+		printf("access pattern = staggered\n");
+	} else if (argv[4][0] == 'r') { //random
+		randomFlag = 1;
+		printf("access pattern = random\n");
+		printf("RAND_MAX = %d\n", RAND_MAX);
+	} else if (argv[4][0] == 'n') { //sequential
+		printf("access pattern = sequential\n");
+		randomFlag = 0;
+		staggerFlag = 0;
+	} else { //bad
+		fprintf(stderr, "Bad access pattern flag\n");
+		return 1;
+	}
 
+	//get arg 5
+	if (argv[5][0] == 'w') { //write
+		writeFlag = 1;
+		printf("access mode = write\n");
+	} else if (argv[5][0] == 'r') { //read
+		writeFlag = 0;
+		printf("access mode = read\n");
+	} else { 
+		fprintf(stderr, "Bad write/read input\n");
+		return 1;
+	}
+
+	//get arg 6
+	numIters = (int)atoi(argv[6]);
+	if (numIters < 1) {
+		fprintf(stderr, "Number of iterations was < 1, setting to 1\n");
+		numIters = 1;
+	}
+	printf("num iterations = %d\n", numIters);
+
+	printf("\n==============================\n\n");
+
+	val = 0xFFFFFFFFFFFFFFFF; //We will deal with purely all 1s in the allocated memory, for consistency.
+	
 	//allocate the huge array for storing page address pointers
-	if (allocator_choice == 2) { //use vip_mmap
+	if (allocator_choice == 2) { //use vip_malloc
+		printf("Getting memory pointer array using malloc...\n");
+		if (!(page_array = vip_malloc(MAX_NUM_PAGES*sizeof(unsigned long *), _VIP_TYP_READ | _VIP_UTIL_HI))) {
+			printf("Error: Could not allocate page array using vip_malloc. Terminating.\n");
+			perror("vip_malloc");
+			return 1;
+		}
+ 	} else if (allocator_choice == 3) { //use vip_mmap
 		printf("Getting memory pointer array using vip_mmap with vip_flags READ/HI...\n");
 		page_array = (unsigned long **) syscall(NR_vip_mmap, NULL, MAX_NUM_PAGES*sizeof(unsigned long *), (PROT_READ | PROT_WRITE | _VIP_TYP_READ | _VIP_UTIL_HI), (MAP_ANONYMOUS | MAP_PRIVATE), -1, 0);
 		if (page_array == MAP_FAILED) {
@@ -113,16 +183,22 @@ int main(const int argc, const char *argv[]) {
 	
 	printf("sizeof(val) == %lu\nsizeof(page_array) == %lu\nsizeof(*(page_array)) == %lu\nsizeof(**page_array) == %lu\n", sizeof(val), sizeof(page_array), sizeof(*page_array), sizeof(**page_array));
 	printf("PAGE_SIZE == %lu\nwordsPerPage == %lu\n", (unsigned long) PAGE_SIZE, (unsigned long) wordsPerPage);
-	
-	MB_to_alloc = (unsigned long) atol(argv[1]); //Be nice, I didn't bother validating input ><
-	sleepTime = (int) atol(argv[2]);
-	if (sleepTime < 0) {
-		fprintf(stderr, "Sleep time was < 0s, setting to 0s.\n");
-		sleepTime = 0;
-	}
+
+	printf("\n==============================\n\n");
 
 	//Hog memory
-	if (allocator_choice == 2) {
+	if (allocator_choice == 2) { //vip_malloc
+		printf("Hogging memory using vip_malloc...\n");
+		while ((page < MAX_NUM_PAGES) && (page < MB_to_alloc*1024*1024/PAGE_SIZE)) {
+			*(page_array+page) = vip_malloc(PAGE_SIZE, _VIP_TYP_READ | _VIP_UTIL_HI);
+			if (*(page_array+page) == NULL) {
+				printf("Error: Failed to vip_malloc page number %lu, at %0.2f MB.\n", page, (double)page*PAGE_SIZE/(1024*1024));
+				perror("vip_malloc");
+				break;
+			}
+			page++;
+		}
+	} else if (allocator_choice == 3) { //vip_mmap
 		printf("Hogging memory using vip_mmap with flags WRITE/HI...\n");
 		while ((page < MAX_NUM_PAGES) && (page < MB_to_alloc*1024*1024/PAGE_SIZE) && (*(page_array+page) = (unsigned long *) syscall(NR_vip_mmap, NULL, PAGE_SIZE, (PROT_READ | PROT_WRITE | _VIP_TYP_WRITE | _VIP_UTIL_HI), (MAP_ANONYMOUS | MAP_PRIVATE), -1, 0)) != MAP_FAILED) 
 			page++;
@@ -140,11 +216,14 @@ int main(const int argc, const char *argv[]) {
 		}
 	} else { //malloc
 		printf("Hogging memory using malloc...\n");
-		while ((page < MAX_NUM_PAGES) && (page < MB_to_alloc*1024*1024/PAGE_SIZE) && (*(page_array+page) = malloc(PAGE_SIZE))) 
+		while ((page < MAX_NUM_PAGES) && (page < MB_to_alloc*1024*1024/PAGE_SIZE)) {
+			*(page_array+page) = malloc(PAGE_SIZE);
+			if (*(page_array+page) == NULL) {
+				printf("Error: Failed to malloc page number %lu, at %0.2f MB.\n", page, (double)page*PAGE_SIZE/(1024*1024));
+				perror("malloc");
+				break;
+			}
 			page++;
-		if (*(page_array+page) == NULL) {
-			printf("Error: Failed to malloc page number %lu, at %0.2f MB.\n", page, (double)page*PAGE_SIZE/(1024*1024));
-			perror("malloc");
 		}
 	}
 
@@ -152,42 +231,12 @@ int main(const int argc, const char *argv[]) {
 	
 	printf("We allocated %lu pages == %lu KB == %0.2f MB == %0.2f GB. Done.\n", total_pages, total_pages*PAGE_SIZE/1024, (double)total_pages*PAGE_SIZE/(1024*1024), (double)total_pages*PAGE_SIZE/(1024*1024*1024));
 	
-	//User: Stagger, random, or sequential?
-	do {
-		printf("Staggered words or random page accesses? (s/r/n)\n");
-		choice = getchar();
-		getchar();
-	} while (choice != 's' && choice != 'r' && choice != 'n');
-	
-	if (choice == 's') { //stagger
-		staggerFlag = 1;
-		printf("Staggered accesses enabled!\n");
-	} else if (choice == 'r') { //random
-		randomFlag = 1;
-		printf("Random accesses enabled!\n");
-		printf("RAND_MAX = %d\n", RAND_MAX);
-	} else if (choice == 'n') { //sequential
-		printf("Sequential accesses enabled!\n");
-	}
-
-	choice = '\0';
-	
-	//User: write or read?
-	do {
-		printf("Type 'w' to infinite write, 'r' to infinite read.\nPress ENTER at any time to stop.\n");
-		choice = getchar();
-		getchar();
-	} while (choice != 'w' && choice != 'r');
-	
-	val = 0xFFFFFFFFFFFFFFFF; //We will deal with purely all 1s in the allocated memory, for consistency.
-	
-	if (choice == 'w')
-		writeFlag = 1;
+	printf("\n==============================\n\n");
 	
 	/************ WORKLOAD ************/
 	if (writeFlag) { //User chose to write
 		printf("Writing pattern: 0x%X %X\n", (unsigned int) (val>>32), (unsigned int) val);
-		while (1) {
+		for (iter = 1; iter <= numIters; iter++) {
 			if (randomFlag) { //randomize pages
 				for (pageCount = 0; pageCount < total_pages; pageCount++) {
 					page = rand() % total_pages;
@@ -220,9 +269,9 @@ int main(const int argc, const char *argv[]) {
 					}
 			}
 			
-			printf("Finished write iteration %d\n", iter++);
-			if (check_for_keyboard_input())
-				break;
+			printf("Finished write iteration %d\n", iter);
+			/*if (check_for_keyboard_input())
+				break;*/
 		
 			//Check sleep mode
 			if (sleepTime > 0)
@@ -237,7 +286,7 @@ int main(const int argc, const char *argv[]) {
 				*(*(page_array+page)+word) = val; //Write all 1s to the word
 		
 		printf("Reading...\n");
-		while (1) {
+		for (iter = 1; iter <= numIters; iter++) {
 			if (randomFlag) { //randomize pages
 				for (pageCount = 0; pageCount < total_pages; pageCount++) {
 					page = rand() % total_pages;
@@ -270,9 +319,9 @@ int main(const int argc, const char *argv[]) {
 					}
 			}
 
-			printf("Finished read iteration %d\n", iter++);
-			if (check_for_keyboard_input())
-				break;
+			printf("Finished read iteration %d\n", iter);
+/*			if (check_for_keyboard_input())
+				break;*/
 			
 			//Check sleep mode
 			if (sleepTime > 0)
@@ -293,8 +342,8 @@ int main(const int argc, const char *argv[]) {
 	}
 	
 	printf("Done.\n");
-	while (check_for_keyboard_input()) //flush stdin
-		getchar();
+	/*while (check_for_keyboard_input()) //flush stdin
+		getchar();*/
 
 	return 0; 
 }
